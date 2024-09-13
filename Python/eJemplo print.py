e4b9ba8f42b9f7d4aca1d9d132a7a97430d8f72c
Rubogato <ruben.zhu@campus.xaloc.org>
2024-09-13 12:32:48 +0200

    Add files via upload

---
+++ Python/eJemplo print.py
@@ -0,0 +1,13 @@
+#2 formas de trabajar con "print"
+
+var_peso=3
+
+
+
+
+#Poner textos entre comillas dentro de print para poder mostrar texto con variables.
+print ("el peso total es de", var_peso, "kg. ¿Quieres una bolsa?")
+
+set, var_pregunta
+
+print (var_pregunta)
