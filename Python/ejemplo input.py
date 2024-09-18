90e7cd0b36f3eb6b1e62002cad18d8355ddecb7f
Rubogato <ruben.zhu@campus.xaloc.org>
2024-09-18 12:55:24 +0200

    Add files via upload

---
+++ Python/ejemplo input.py
@@ -0,0 +1,9 @@
+#input se usa para que el usuario pueda poner variables de texto
+# int convierte el texto en numeros enteros
+#float permite usar numeros decimales
+#str se utiliza para convertir variables en texto
+var1=int(input("introduce el primer numero: "))
+var2=float(input("introduce el segundo numero: "))
+
+var3= var1+var2
+print("La suma da un total de ",var3)
