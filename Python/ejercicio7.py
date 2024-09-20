34939999dab6cc3dc50d65e48362d988f4959688
Rubogato <ruben.zhu@campus.xaloc.org>
2024-09-20 12:11:01 +0200

    Add files via upload

---
+++ Python/ejercicio7.py
@@ -0,0 +1,14 @@
+valor1=int(input("Operador1: "))
+valor2=int(input("Operador2: "))
+totalsuma=valor1+valor2
+totalresta=valor1-valor2
+totalmulti=valor1*valor2
+totaldivi=valor1/valor2
+totalexpo=valor1**valor2
+totaldivicompleta=valor1//valor2
+print("La suma de operador1 y operador2 es: ",totalsuma)
+print("La resta de operador1 y operador2 es: ",totalresta)
+print("La multiplicacion de operador1 y operador2 es: ",totalmulti)
+print("La division de operador1 y operador2 es: ",round(totaldivi,2))
+print("El exponente de operador1 y operador2 es: ",totalexpo)
+print("La division entera de operador1 y operador2 es: ",totaldivicompleta)
