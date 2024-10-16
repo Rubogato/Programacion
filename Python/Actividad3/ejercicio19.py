5f5e28567e554ca5a6d71a5d817ecef75e19b787
Rubogato <ruben.zhu@campus.xaloc.org>
2024-10-16 12:51:16 +0200

    Add files via upload

---
+++ Python/Actividad3/ejercicio19.py
@@ -0,0 +1,8 @@
+var1=int(input("Dame un valor: "))
+var2=int(input("Dame otro valor: "))
+if var1>var2:
+    print("El número",var1,"es mayor al número",var2)
+elif var1<var2:
+    print("El número",var2,"es mayor al número",var1)
+else:
+    print("Los dos números son iguales")
