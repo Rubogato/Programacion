eef9ae6f555376db10fcf639d7df6c0e73c9fb9f
Rubogato <ruben.zhu@campus.xaloc.org>
2024-10-23 13:19:58 +0200

    Delete Python/Actividad3/Sin título3.py

--- Python/Actividad3/Sin título3.py
+++
@@ -1,6 +0,0 @@
-#Busca Internet qué función permite obtener la longitud de un String, realiza un programa 
-#que al introducir una frase devuelva la longitud
-string=input("Introduce la frase:")
-a=string.count("")
-b=a-1
-print("La longitud de la frase es",b)
